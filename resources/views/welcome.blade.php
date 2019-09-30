d5cd0eadbf70adceaa479e342f9c5bb06e70118b
Yajendra <yajendra@truelancer.com>
2019-09-30 23:22:14 +0530

    Dockerfile Setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel with Docker
                 </div>
 
                 <div class="links">
