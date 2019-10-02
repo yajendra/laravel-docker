04f85ab2eec06a13c84ceca41e0e298a725c8a74
Yajendra <yajendra@truelancer.com>
2019-10-02 17:06:05 +0530

    Docker Redis

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +13,15 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/users' , function(){
+	$users = Cache::rememberForever('user-list',function(){
+		return User::all();
+	});
+	foreach($users as $user){		
+		echo $user->id .' '.$user->name .'  -  '. $user->email .' - '.$user->created_at .' -  '. $user->updated_at;
+		echo '<br/>';
+	}
 });
